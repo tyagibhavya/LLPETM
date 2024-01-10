790a7873e15f4271d3dd5970df65949261ed5dc4
Aditya Bawangade <105143252+Aditya-Bawangade@users.noreply.github.com>
2024-01-10 11:19:51 +0530

    Final* versions of types and macros header files

--- common/types.h
+++ common/types.h
@@ -22,7 +22,7 @@ namespace Common{
     typedef uint64_t OrderId;
     /*we define the OrderId type to identify orders,
     which is simply uint64_t, and a corresponding orderIdToString() 
-    method to log it. We also add a OrderId_INVALID sentinel 
+    method to log it. We also add a OrderId_INVALID  
     method to signify invalid values
     */
     constexpr auto OrderId_INVALID = std::numeric_limits<OrderId>:: max;
@@ -59,4 +59,94 @@ namespace Common{
    }
 
    /* Same pattern followed as the previous inline function*/
+
+   typedef uint32_t ClientId ;
+   /*
+   We define the TickerId type to identify trading instruments, which is
+   simply a uint32_t type, and add a corresponding tickerIdToString() method for it. 
+   We have a TickerId_INVALID sentinel value for invalid instruments
+   */
+   constexpr auto ClientId_INVALID = std::numeric_limits<ClientId>::max();
+    inline auto clientIdToString(ClientId client_id) ->std::string {
+    if (UNLIKELY(client_id == ClientId_INVALID)) {
+      return "INVALID";
+    }
+    return std::to_string(client_id);
+  }
+
+  /* Same pattern of code as above */
+
+
+
+    typedef int64_t Price;
+    /*
+    The next type is Price, which is used to capture prices
+    on orders. We also add a Price_INVALID constant to represent 
+    invalid prices. Finally, a priceToString() method to stringify these values
+    */
+    constexpr auto Price_INVALID = std::numeric_limits<Price>::max();
+    inline auto priceToString(Price price) -> std::string {
+    if (UNLIKELY(price == Price_INVALID)) {
+      return "INVALID";
+    }
+    return std::to_string(price);
+  }
+    /* Similar to the previous code pattern*/
+
+
+
+    typedef uint32_t Qty;
+    /*
+    The Qty type is typedef for uint32_t and represents order quantities.
+    We also provide the usual Qty_INVALID sentinel and the qtyToString() method
+    to convert them to strings
+    */
+    constexpr auto Qty_INVALID = std::numeric_limits<Qty>::max();
+    inline auto qtyToString(Qty qty) -> std::string {
+        if (UNLIKELY(qty == Qty_INVALID)) {
+        return "INVALID";
+        }
+        return std::to_string(qty);
+    }
+    /* Same as above */
+
+
+
+    typedef uint64_t Priority;
+    /*
+    The Priority type is just a position in the queue of type uint64_t.
+    We assign the Priority_INVALID sentinel value and the priorityToString() method
+    */
+    constexpr auto Priority_INVALID = std::numeric_limits<Priority>::max();
+
+    inline auto priorityToString(Priority priority) -> std::string {
+        if (UNLIKELY(priority == Priority_INVALID)) {
+        return "INVALID";
+        }
+
+        return std::to_string(priority);
+    }
+    /* Same as above */
+
+    enum class Side : int8_t {
+        INVALID = 0, 
+        BUY = 1,
+        SELL = -1
+    };
+    /*
+    The Side type is an enumeration and contains two valid values, as shown in 
+    the following code block. We also define a sideToString() method, as
+    we did for the other types previously
+    */
+    inline auto sideToString(Side side) -> std::string {
+        switch (side) {
+        case Side::BUY:
+            return "BUY";
+        case Side::SELL:
+            return "SELL";
+        case Side::INVALID:
+            return "INVALID";
+        }
+        return "UNKNOWN";
+    }    
 }
