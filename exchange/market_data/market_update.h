75503eef89860e03a8d3265f35d7dc59c7b927db
Aditya Bawangade <105143252+Aditya-Bawangade@users.noreply.github.com>
2024-01-11 23:00:03 +0530

    Aditya : Commiting for updating

--- exchange/market_data/market_update.h
+++ exchange/market_data/market_update.h
@@ -9,6 +9,11 @@ type to represent a lock-free queue of the MEMarketUpdate objects
 #include "common/types.h"
 using namespace Common ;
 namespace Exchange{
+    /*
+   The MEMarketUpdate struct also needs to be a packed structure, 
+   since it will be part of the message that is sent and received over the network; hence,
+   we use the #pragma pack() directive again
+    */
     #pragma pack(push,1)
     enum class MarketUpdateType : uint8_t {
         INVALID = 0,
@@ -17,12 +22,43 @@ namespace Exchange{
         CANCEL = 3,
         TRADE = 4
     } ;
+
     inline std:: string marketUpdateTypeToString(MarketUpdateType type) {
         switch(type){
             case MarketUpdateType::INVALID: return "INVALID" ;
             case MarketUpdateType::ADD: return "ADD" ;
             case MarketUpdateType::TRADE: return "TRADE";
-            
+            case MarketUpdateType::CANCEL: return "CANCEL";
         }
+        return "UNKNOWN" ;
+    }
+
+    struct MEMarketUpdate{
+        MarketUpdateType type_ = MarketUpdateType::INVALID;
+        OrderId order_id_ = OrderId_INVALID;
+        TickerId ticker_id_ = TickerId_INVALID;
+        Side side_ = Side::INVALID;
+        Price price_ = Price_INVALID;
+        Qty qty_ = Qty_INVALID;
+        Priority priority_ = Priority_INVALID;
+        /*
+        A priority_ field of type Priority, which, as we
+        discussed before, will be used to specify the exact position of this order in 
+        the FIFO queue. We build a FIFO queue of all orders at the same price. This
+        field specifies the position/location of this order in that queue.
+        */
+        auto toString() const {
+        std::stringstream ss;
+        ss << "MEMarketUpdate"
+            << " ["
+            << " type:" << marketUpdateTypeToString(type_)
+            << " ticker:" << tickerIdToString(ticker_id_)
+            << " oid:" << orderIdToString(order_id_)
+            << " side:" << sideToString(side_)
+            << " qty:" << qtyToString(qty_)
+            << " price:" << priceToString(price_)
+            << " priority:" << priorityToString(priority_)
+            << "]";
+        return ss.str();
     }
 }
