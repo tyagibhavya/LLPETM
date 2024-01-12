7204018a930dd5d20e74ea9c8c056cf0db05a581
Aditya Bawangade <105143252+Aditya-Bawangade@users.noreply.github.com>
2024-01-12 09:40:39 +0530

    Aditya : Modified matching_engine.h and .cpp files

--- exchange/matcher/matching_engine.h
+++ exchange/matcher/matching_engine.h
@@ -10,56 +10,69 @@ Not read yet
 */
 #include "me_order_book.h"
 
+namespace Exchange
+{
 
-namespace Exchange{
+    class MatchingEngine Final
+    {
+    public:
+        MatchingEngine(ClientLFQueue *client_requests,
+                       ClientLFQueue *client_responses, MEMarketUpdateLFQueue *market_updates);
+        ~MatchingEngine();
+        auto start() -> void;
+        auto stop() -> void;
+        
 
-    class MatchingEngine Final {
-        public:
-            MatchingEngine(ClientLFQueue *client_requests,
-            ClientLFQueue *client_responses, MEMarketUpdateLFQueue *market_updates) ;
-            ~MatchingEngine();
-            auto start()->void;
-            auto stop()->void;
-            MatchingEngine() = delete;
+        MatchingEngine() = delete;
 
-            /*Copy constructor disabled*/
-            MatchingEngine(const MatchingEngine &) = delete;
-            /*Move constructor disabled*/
-            MatchingEngine(const MatchingEngine &&) = delete;
-            /*
-            The move assignment operator below :
-            It performs a similar role as the move constructor but is used when the object
-            already exists. The move assignment operator efficiently transfers ownership of
-            resources from source to destination. After the move, source is in a valid but unspecified
-            state, and destination has taken ownership of the resources.
-            */
+        /*Copy constructor disabled*/
+        MatchingEngine(const MatchingEngine &) = delete;
+        /*Move constructor disabled*/
+        MatchingEngine(const MatchingEngine &&) = delete;
+        /*
+        The move assignment operator below :
+        It performs a similar role as the move constructor but is used when the object
+        already exists. The move assignment operator efficiently transfers ownership of
+        resources from source to destination. After the move, source is in a valid but unspecified
+        state, and destination has taken ownership of the resources.
+        */
 
-            MatchingEngine &operator = (const MatchingEngine &) = delete ;
-            MatchingEngine &operator = (const MatchingEngine &&)= delete;
+        MatchingEngine &operator=(const MatchingEngine &) = delete;
+        MatchingEngine &operator=(const MatchingEngine &&) = delete;
 
-            private:
-                OrderBookHashMap ticker_order_book_;
-                ClientRequestLFQueue *incoming_requests_ = nullptr;
-                ClientResponseLFQueue *outgoing_ogw_responses_ =
-                nullptr;
-                MEMarketUpdateLFQueue *outgoing_md_updates_ = nullptr;
-                volatile bool run_ = false;
-                std::string time_str_;
-                Logger logger_;
+    private:
+        OrderBookHashMap ticker_order_book_;
+        ClientRequestLFQueue *incoming_requests_ = nullptr;
+        ClientResponseLFQueue *outgoing_ogw_responses_ = nullptr;
+        MEMarketUpdateLFQueue *outgoing_md_updates_ = nullptr;
+        volatile bool run_ = false;
+        /*
+        The volatile keyword is used here to indicate that the value of run_ might
+        change at any time without any action being taken by the code that the compiler
+        finds nearby. This is often used when the variable can be modified by hardware,
+        other threads, or interrupt service routines.
 
-        };
-}
+        The significance of volatile in more detail:
 
-#include "matching_engine.h"
-namespace Exchange{
-    MatchingEngine::MatchingEngine(ClientRequestLFQueue
-    *client_requests, ClientResponseLFQueue
-    *client_responses, MEMarketUpdateLFQueue
-    *market_updates):incoming_requests_(client_requests),
-        outgoing_ogw_responses_(client_responses),
-        outgoing_md_updates_(market_updates),
-        logger_("exchange_matching_engine.log"){
-            for(size_t i = 0; i < ticker_order_book_.size(); ++i) {
-                ticker_order_book_[i] = new MEOrderBook(i, &logger_, this);
-    }
+        Prevents Compiler Optimization: When a variable is declared as volatile, the compiler is instructed not
+        to perform certain optimizations that might assume the variable's value doesn't change outside the
+        current code path. This is crucial for variables that can be modified by external entities like hardware.
+
+        Forces Every Access: Each time the variable is accessed, the compiler generates code to actually read
+        the value from memory and not rely on any cached values. Similarly, when the variable is modified, the
+        new value is written directly to memory.
+
+        Use Cases: volatile is often used in situations where a variable is shared among different parts
+        of a program or between a program and an external entity (like hardware). For example, a flag indicating
+        whether a program should continue running (as in your run_ example) might be modified by an interrupt
+        service routine or another thread.
+
+        Here, the use of volatile indicates to the compiler that it should not make assumptions about
+        the stability of the run_ variable and should always fetch its current value from memory. This is
+        crucial in scenarios where the variable can be changed asynchronously, outside the normal flow of
+        the program.
+        */
+        std::string time_str_;
+        Logger logger_;
+    };
 }
