6837258d4317bf523e3f85361b2737e525cbd02f
Bhavya Tyagi <99269005+tyagibhavya@users.noreply.github.com>
2024-01-13 14:59:55 +0530

    bhavya: updated client_request.h

--- exchange/order_server/client_request.h
+++ exchange/order_server/client_request.h
@@ -63,6 +63,25 @@ namespace Exchange{
   
    };
 
+  /*
+     Client request structure published over the network by the order gateway client.
+     OMClientRequest has a sequence number field (seq_num_)
+*/
+  struct OMClientRequest {
+    size_t seq_num_ = 0;
+    MEClientRequest me_client_request_;
+
+    auto toString() const {
+      std::stringstream ss;
+      ss << "OMClientRequest"
+         << " ["
+         << "seq:" << seq_num_
+         << " " << me_client_request_.toString()
+         << "]";
+      return ss.str();
+    }
+  };
+
    #pragma pack(pop)
    typedef LFQueue<MEClientRequest> ClientRequestLFQueue ;
 
